6600fbc48bc1147c484be39fe22f3a93627c4247
uzii9 <70870057+uzii9@users.noreply.github.com>
2020-09-06 20:30:59 -0600

    Add files via upload

---
+++ MadLibb.py
@@ -0,0 +1,29 @@
+start = input("Hello! Welcome to my first MadLibs game! Let's start off by getting your name summoner!(Press Enter to continue)")
+summoner_name = input("Enter summoner name: ")
+print (" Hello " + summoner_name + " Welcome!, your goal is to fill out the following Madlib! ")
+print( )
+print ("____ is _____")
+print ("_____ are annoying")
+print ("____ is hard")
+print ( )
+
+start2 =  input(" Good luck " + summoner_name + " ! (Press Enter to continue)")
+
+
+
+Champion1 = input("Choose a champion: ")
+print ( )
+Champion_Difficulty = input("How hard is your champion? [Easy/Medium/Hard]: ")
+print ( )
+Champion = input("Choose another LoL champion: ")
+print ( )
+Plural_Noun = input("Choose a plural noun: ")
+print ( )
+
+
+
+print (Champion1 + " is " + Champion_Difficulty)
+print (Plural_Noun + " are annoying")
+print (Champion + " is hard")
+print (  )
+print ("Thank you for playing")
